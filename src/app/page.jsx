85ede01624b78821eabfc2195eaceea074f19f25
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-07-30 22:47:19 -0300

    ok
    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
 
-    <h1>Aula Next</h1>
+    <div className='container'>
+      <h1 className={styles.titulo}>Aula Next</h1>
+    </div>
+
   );
 }
